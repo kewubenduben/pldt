1abc96902eeed3a0eb0127b14cd45a89bb0cf2d2
Ker Ruben Ramos <xdiscent@gmail.com>
2020-01-09 05:15:04 +0000

    Add js sanitizer

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,45 @@
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
 
-const minifyJSOption = {
+gulp.task('sanitize-asp', () => {
+  const minifyJSOption = {
     parse: {},
     compress: false,
     mangle: false,
     output: {
-        comments: false,
-        beautify: true,
-        preserve_line: true
+      comments: false,
+      beautify: true,
+      preserve_line: true,
+      keep_quoted_props: true,
+      braces: true
     }
-};
+  };
 
-gulp.task('minify', () => {
-  return gulp.src('**/web/**/*.asp')
-    .pipe(htmlmin({ minifyJS: true }))
+  return gulp.src('H*/**/web/**/*.asp')
+    .pipe(htmlmin({ minifyJS: minifyJSOption }))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('sanitize-js', () => {
+  const minifyJSOption = {
+    parse: {},
+    compress: false,
+    mangle: false,
+    output: {
+      comments: false,
+      beautify: false,
+      preserve_line: true,
+      keep_quoted_props: true,
+      braces: true
+    }
+  };
+
+  return gulp.src('H*/**/web/**/*.js')
+    .pipe(uglify(minifyJSOption))
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('sanitize', gulp.parallel('sanitize-asp', 'sanitize-js'));
 
